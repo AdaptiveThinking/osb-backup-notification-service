495156c98070afe546ed458b2d397d1642592a64
Thomas Quandt <thquad@gmail.com>
2021-09-17 16:21:26 +0200

    feat(uaa): add a functioning UAA

---
+++ osb-backup-notification-service/src/main/java/de/evoila/cf/notification/UAASecurityConfiguration.java
@@ -0,0 +1,26 @@
+package de.evoila.cf.notification;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@ConditionalOnProperty(name = "spring.security.oauth2.resourceserver.jwt.issuer-uri")
+public class UAASecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers()
+                .antMatchers("/**")
+                .and()
+                .cors()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+}
